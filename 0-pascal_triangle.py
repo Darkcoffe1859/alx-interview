44bad226e0c1a867c22c65c41b25725764d849bd
Darkcoffe1859 <shency49@gmail.com>
2023-11-30 22:25:33 +0100

    pascal

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python3
-def pascal_triangle(n):
+"""
+Defines function that returns a list of integers
+representing the pascal's triangle of n
+"""
+
+
+def pascal_trangle(n):
+    """
+    Creates a list of lists of integers representing Pascal's triangle
+
+    parameters:
+    n [int]:
+    the number of rows of Pascal's triangle to recreate
+
+    return:
+    [liist of lists of ints]:
+    representation of Pascal's triangle
+    """
+    if type(n) is not int:
+        raise TypeError("n must be an integer")
+    triangle = []
     if n <= 0:
-        return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        prev_row = triangle[i - 1]
-        new_row = [1]
-
-        for j in range(1, i):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-
-            new_row.append(1)
-            triangle.append(new_row)
-
-            return triangle
-        #usage
-        if __name__ == "__main__":
-            triangle = pascal_triangle(5)
-            print_triangle(triangle)
-
+        return triangle
+    previous = [1]
+    for row_index in range(n):
+        rowlist = []
+        if row_index == 0
+        rowlist = [1]
+    else:
+        for i in range(row_index + 1):
+            if i == 0:
+                rowlist.append(0 + previous[i])
+            elif i == (row_index):
+                rowlist.append(previous[i - 1] + 0)
+            else:
+                rowlist.append(previous[i - 1] + previous[i])
+                previous = rowlist
+                triangle.append(rowlist)
+                return triangle
