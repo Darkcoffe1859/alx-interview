7cce8a23a0b630c97d9e16f6a346a67be6d66298
Darkcoffe1859 <shency49@gmail.com>
2023-11-30 13:48:44 +0100

    updated

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""0-pascal_triangle
-"""
-
-
 def pascal_triangle(n):
-    """_summary_
-
-    Args:
-    n (integer): number of rows
-
-    Returns:
-    list: paslcal triangle
-    """
     if n <= 0:
         return []
-    out = [[1]]
-    for i in range(n - 1):
-        temp = [0] + out[-1] + [0]
-        row = []
-        for j in range(len(out[-1]) + 1):
-            row.append(temp[j] + temp[j + 1])
-            out.append(row)
-            return out
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        new_row = [1]
+
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+            new_row.append(1)
+            triangle.append(new_row)
+
+            return triangle
+
+
